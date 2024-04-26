30f0693f5de58aea20f46880a58177343a3cf989
Manuel Alonso Carracedo <manuelalonsocarracedo@gmail.com>
2024-04-26 03:20:08 +0200

    addds mssql firewall rule from aci to db

--- data-resources.tf
+++ data-resources.tf
@@ -33,6 +33,13 @@ resource "azurerm_mssql_database" "wilde-mssql-app-db" {
 
 // PRIVATE NETWORKING 
 
+resource "azurerm_mssql_firewall_rule" "AllowTrustedAzureServices" {
+  name             = "AllowTrustedAzureServices"
+  server_id        = azurerm_mssql_server.wilde-mssql-server.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
+
 resource "azurerm_private_dns_zone" "wilde-data-dns-zone" {
   name                = "${var.private_dns_zone_name}.database.windows.net"
   resource_group_name = azurerm_resource_group.wilde-data-rg.name
@@ -45,13 +52,32 @@ resource "azurerm_private_dns_zone_virtual_network_link" "wilde-data-dns-zone-li
   virtual_network_id    = azurerm_virtual_network.wilde-common-vnet.id
 }
 
-module "wilde-mssql-server-endpoint" {
-  source                             = "./tf_modules/private_endpoint"
-  location                           = azurerm_resource_group.wilde-data-rg.location
-  resource_group_name                = azurerm_resource_group.wilde-data-rg.name
-  private_link_enabled_resource_name = azurerm_mssql_server.wilde-mssql-server.name
-  private_link_enabled_resource_id   = azurerm_mssql_server.wilde-mssql-server.id
-  subnet_id                          = azurerm_subnet.wilde-subnets["wilde-data-subnet"].id
-  subresource_names                  = ["sqlServer"]
-  private_dns_zone_id                = azurerm_private_dns_zone.wilde-data-dns-zone.id
+# module "wilde-mssql-server-endpoint" {
+#   source                             = "./tf_modules/private_endpoint"
+#   location                           = azurerm_resource_group.wilde-data-rg.location
+#   resource_group_name                = azurerm_resource_group.wilde-data-rg.name
+#   private_link_enabled_resource_name = azurerm_mssql_server.wilde-mssql-server.name
+#   private_link_enabled_resource_id   = azurerm_mssql_server.wilde-mssql-server.id
+#   subnet_id                          = azurerm_subnet.wilde-subnets["wilde-data-subnet"].id
+#   subresource_names                  = ["sqlServer"]
+#   private_dns_zone_id                = azurerm_private_dns_zone.wilde-data-dns-zone.id
+# }
+
+resource "azurerm_private_endpoint" "endpoint" {
+  name                = format("%s-%s", azurerm_mssql_server.wilde-mssql-server.name, "endpoint")
+  location            = azurerm_resource_group.wilde-data-rg.location
+  resource_group_name = azurerm_resource_group.wilde-data-rg.name
+  subnet_id           = azurerm_subnet.wilde-subnets["wilde-data-subnet"].id
+
+  private_dns_zone_group {
+    name                 = format("%s-%s", azurerm_mssql_server.wilde-mssql-server.name, "privatednszonegroup")
+    private_dns_zone_ids = [azurerm_private_dns_zone.wilde-data-dns-zone.id]
+  }
+
+  private_service_connection {
+    name                           = format("%s-%s", azurerm_mssql_server.wilde-mssql-server.name, "privateserviceconnection")
+    private_connection_resource_id = azurerm_mssql_server.wilde-mssql-server.id
+    is_manual_connection           = false
+    subresource_names              = ["sqlServer"]
+  }
 }

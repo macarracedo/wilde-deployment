d86b9943dff4ff22a179a0317fcedd0dbfb3c655
Manuel Alonso Carracedo <manuelalonsocarracedo@gmail.com>
2024-04-26 03:19:43 +0200

    adds 2 variables connection string

--- locals.tf
+++ locals.tf
@@ -1,3 +1,7 @@
 locals {
-  sqlalchemy_connection_string = "mssql+pyodbc://${var.mssql-admin}:${var.mssql-admin-pass}@${azurerm_mssql_server.wilde-mssql-server.fully_qualified_domain_name}/${azurerm_mssql_database.wilde-mssql-app-db.name}?driver=ODBC+Driver+18+for+SQL+Server&trustServerCertificate=no"
+  sqlalchemy_connection_string_1 = "mssql+pyodbc://${var.mssql-admin}:${var.mssql-admin-pass}@${azurerm_mssql_server.wilde-mssql-server.fully_qualified_domain_name}/${azurerm_mssql_database.wilde-mssql-app-db.name}?driver=ODBC+Driver+18+for+SQL+Server&trustServerCertificate=no"
+}
+
+locals {
+  sqlalchemy_connection_string_2 = "mssql+pymssql://${var.mssql-admin}:${var.mssql-admin-pass}@${azurerm_mssql_server.wilde-mssql-server.fully_qualified_domain_name}/${azurerm_mssql_database.wilde-mssql-app-db.name}?charset=utf8"
 }
